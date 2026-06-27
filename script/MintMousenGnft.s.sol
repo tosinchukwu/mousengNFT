68c8b6e3175e1b172dec6820fa50a7d1ea70a85a
Tosinchukwu <tosin.mouseng20@gmail.com>
2026-06-27 14:35:51 +0100

    Mint NFT

---
+++ script/MintMousenGnft.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/MousengNFT.sol";
+
+contract MintMousengNFT is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        MousengNFT nft = MousengNFT (0xA267C1f337dbe9a2497fC8d2405227F98Fe64427);
+       nft.mint(0x00dC0454E2FdE88f43F1583657721F5008D35C45);
+
+        vm.stopBroadcast();
+    }
+}
